78af8e8ffaae73f52291cbb67a0fa398189f0aab
Samuele Maci <macisamuele@gmail.com>
2019-04-25 11:05:58 +0200

    Implement internal loader cache

---
+++ src/cache/stats.rs
@@ -0,0 +1,36 @@
+use crate::cache::Stats;
+
+impl Default for Stats {
+    fn default() -> Self {
+        Self { hits: 0, misses: 0 }
+    }
+}
+
+#[allow(unsafe_code)]
+unsafe impl Send for Stats {}
+
+#[allow(unsafe_code)]
+unsafe impl Sync for Stats {}
+
+impl Stats {
+    pub(crate) fn register_hit(&mut self) {
+        self.hits += 1;
+    }
+
+    pub(crate) fn register_miss(&mut self) {
+        self.misses += 1;
+    }
+
+    pub(crate) fn get_hits(&self) -> usize {
+        self.hits
+    }
+
+    pub(crate) fn get_misses(&self) -> usize {
+        self.misses
+    }
+
+    pub(crate) fn clear(&mut self) {
+        self.hits = 0;
+        self.misses = 0;
+    }
+}
