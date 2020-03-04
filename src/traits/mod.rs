c1c7ebea13950b5c6cd2ac5685c276e0f3e8a9a2
Samuele Maci <macisamuele@gmail.com>
2020-03-04 21:47:15 +0100

    Add few tests to ensure that Loader<T> does actually implement LoaderTrait<T>

--- src/traits/mod.rs
+++ src/traits/mod.rs
@@ -10,7 +10,6 @@ pub mod _serde_yaml;
 pub mod rust_type;
 
 pub mod loaders {
-    #[allow(unused_imports)]
     use crate::Loader;
 
     #[cfg(feature = "trait_json")]
@@ -23,4 +22,42 @@ pub mod loaders {
     pub type SerdeYamlLoader = Loader<serde_yaml::Value>;
 
     pub type RustTypeLoader = Loader<::json_trait_rs::RustType>;
+}
+
+#[cfg(test)]
+mod test_loaders_do_implement_loader_trait {
+    #[cfg(feature = "trait_json")]
+    use crate::traits::loaders::JsonLoader;
+    #[cfg(feature = "trait_serde_json")]
+    use crate::traits::loaders::SerdeJsonLoader;
+    #[cfg(feature = "trait_serde_yaml")]
+    use crate::traits::loaders::SerdeYamlLoader;
+    use crate::{traits::loaders::RustTypeLoader, LoaderTrait};
+    use json_trait_rs::{JsonType, RustType};
+
+    #[allow(dead_code)]
+    fn check<T: JsonType<T> + Into<RustType>, L: LoaderTrait<T>>(_loader: &L) {}
+
+    #[cfg(feature = "trait_json")]
+    #[test]
+    fn test_json_loader() {
+        check(&JsonLoader::default());
+    }
+
+    #[cfg(feature = "trait_serde_json")]
+    #[test]
+    fn test_serde_json_loader() {
+        check(&SerdeJsonLoader::default());
+    }
+
+    #[cfg(feature = "trait_serde_yaml")]
+    #[test]
+    fn test_serde_yaml_loader() {
+        check(&SerdeYamlLoader::default());
+    }
+
+    #[test]
+    fn test_rust_type_loader() {
+        check(&RustTypeLoader::default());
+    }
 }
